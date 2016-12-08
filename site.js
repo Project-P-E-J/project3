ecbeab131a60a8827d6cfb7de1d9ef294b8861e3
Chip Pickett <rpickett@hawk.iit.edu>
2016-12-07 19:20:43 -0600

    added a zoom level to the map for when it returns to display the rescue locations; removed commented and unused code

--- site.js
+++ site.js
@@ -1,9 +1,8 @@
-
-
 $(document).ready(function() {
+
   var initLat = 41.8403395;
   var initLong = -87.627072;
-  // var pinImage = new google.maps.MarkerImage("http://www.googlemapsmarkers.com/v1/009900/");
+
   var map = new GMaps({
     el: '#map',
     lat: initLat,
@@ -48,15 +47,15 @@ $(document).ready(function() {
         var longitude = data.results[0].geometry.location.lng;
         var latitude = data.results[0].geometry.location.lat;
         var city = data.results[0].address_components[1].short_name;
-        // console.log(city);
+
         $('#zip').append(': Longitude: ' + longitude + ', Latitude: ' + latitude + '.  City: ' + city);
         map = new GMaps({
           el: '#map',
           lat: latitude,
           lng: longitude,
+          zoom: 9
         });
-        // var cityName = {"city": city};
-        // console.log(cityName);
+
         map.addMarker({
           lat: latitude,
           lng: longitude,
@@ -79,25 +78,6 @@ $(document).ready(function() {
     console.log(zip);
     accessData();
 
-
-    // function someFunction(result) {
-    //   var shelters = result.petfinder.shelters.shelter;
-    //   console.log(shelters);
-
-    //   var localShelters = [];
-    //   for(var i = 0;  i < shelters.length; i++) {
-    //     var shelter = new Object();
-    //     shelter.name = shelters[i].name;
-    //     shelter.city = shelters[i].city;
-    //     shelter.latitude = shelters[i].latitude;
-    //     shelter.longitude = shelters[i].longitude;
-    //     shelter.zip = shelters[i].zip;
-    //     shelter.phone = shelters[i].phone;
-    //     localShelters.push(shelter);
-    //     console.log(shelter.latitude, shelter.longitude);
-    //   }
-    // };
-
     function accessData ()  {
       var key = 'ca77abb4e1f8c1cb6d536dfcdf9f45da';
     // var secret = '68426d0a9aa189d3d8fddb057a27b23d';
@@ -109,10 +89,6 @@ $(document).ready(function() {
     var apiString = 'https://api.petfinder.com/shelter.find?format=json&key=' + key  +
     '&location=' + zip + '&callback=?';
 
-    // var status;
-    // var petData;
-    console.log(apiString);
-
     $.ajax({
       url: apiString,
       dataType: 'jsonp',
@@ -122,10 +98,7 @@ $(document).ready(function() {
         $('#error').append(str.fontcolor("red"));
       },
       success: function(data){
-
       var shelters = data.petfinder.shelters.shelter;
-      // console.log(shelters);
-
       var localShelters = [];
       for(var i = 0;  i < shelters.length; i++) {
         map.addMarker({
@@ -137,19 +110,11 @@ $(document).ready(function() {
             alert('you clicked');
           }
         });
-
-        var shelter = new Object();
-        shelter.name = shelters[i].name.$t;
-        shelter.city = shelters[i].city.$t;
-        shelter.latitude = shelters[i].latitude.$t;
-        shelter.longitude = shelters[i].longitude.$t;
-        shelter.zip = shelters[i].zip.$t;
-        shelter.phone = shelters[i].phone.$t;
-        localShelters.push(shelter).$t;
-        // console.log(shelter.latitude.$t, shelter.longitude.$t);
         }
       }
+
     });
+
   }
 }
 
